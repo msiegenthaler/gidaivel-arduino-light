5f3e7bd16d8fb4839bdc8995cf401c3e22120edf
msiegenthaler <msiegenthaler@gmx.net>
2010-05-28 21:36:41 +0200

    fixed minor bugs in arduino xbee and add debugging output

--- main.cpp
+++ main.cpp
@@ -1,62 +1,80 @@
 #include "WProgram.h"
 #include "arduino-xbee/Series1XBee.h"
-#include "arduino-xbee/ApiModeXBee.h"
-#include "arduino-xbee/NewSoftSerial/NewSoftSerial.h"
-#include "OnOffLightService.h"
-#include "Avieul.h"
+#include "arduino-xbee/NewSoftSerialApiModeXBee.h"
+//#include "OnOffLightService.h"
+//#include "Avieul.h"
 
 // general definitions
 extern "C" void __cxa_pure_virtual(void);
 void __cxa_pure_virtual(void) {}
 
-// New and delete
 void * operator new(size_t size);
-void operator delete(void * ptr);
 void * operator new(size_t size) {
    return malloc(size);
-}
-void operator delete(void * ptr) {
-	//TODO call destructor?
-	free(ptr);
 }
 // end of general definitions
 
 
 #define DEBUG_PRINTS 1
 
+
 // prototypes
 void blink(int n, int d);
 
 // variables
 int ledPin = 13; // LED connected to digital pin 13
-int xbeeRcvPin = 8;
-int xbeeSendPin = 9;
+int xbeeRcvPin = 2;
+int xbeeSendPin = 3;
 
-NewSoftSerial serial = NewSoftSerial(xbeeRcvPin, xbeeSendPin, false);
-ApiModeXBee lowlevel = ApiModeXBee(&serial);
-Series1XBee xbee = Series1XBee(&lowlevel);
-OnOffLightService onOff = OnOffLightService(13);
-AvieulService* services[] = { &onOff };
-Avieul avieul = Avieul(&xbee, services, 1);
+NewSoftSerial *serial;
+LowlevelXBee *lowlevel;
+Series1XBee *xbee;
+//Avieul *avieul;
 
 void setup() {
 #ifdef DEBUG_PRINTS
 	Serial.begin(9600);
 #endif
 
+	serial = new NewSoftSerial(xbeeRcvPin, xbeeSendPin);
+	serial->begin(19200);
+	lowlevel = new NewSoftSerialApiModeXBee(serial);
+	xbee = new Series1XBee(lowlevel);
+/*
+	OnOffLightService *onOff = new OnOffLightService(13);
+	AvieulService* services[] = { onOff };
+	avieul = new Avieul(xbee, services, 1);
+*/
 	pinMode(ledPin, OUTPUT); // sets the digital pin as output
+	digitalWrite(ledPin, HIGH);
 
 #ifdef DEBUG_PRINTS
 	Serial.println("Started");
 #endif
 }
 
-void loop() {
-	avieul.process();
 
-#ifdef DEBUG_PRINTS
-	Serial.println("Loop");
-#endif
+void printHex(uint8_t *data, uint8_t len, bool seperate);
+void printAddress(uint64_t address);
+
+void loop() {
+	bool oldVal = digitalRead(ledPin) == HIGH;
+	digitalWrite(ledPin, oldVal ? LOW : HIGH);
+
+	if (xbee->available()) {
+		XBeeAddress address;
+		uint8_t *data;
+		uint8_t len;
+		xbee->receive(&address, &data, &len);
+
+		Serial.print("incoming message '");
+		printHex(data, len, true);
+		Serial.print("' from ");
+		printAddress(address);
+		Serial.println();
+	}
+
+	delay(500);
 }
 
 
